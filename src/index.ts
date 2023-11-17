12a7bec2d69e2ec2eb7b58470068bb2111a3f00a
Donggi Kim (wiz) <hi.donggi@gmail.com>
2023-11-17 23:04:05 +0900

    eslint 적용

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,13 @@
-function LogMethod(
-    target: any,
-    propertyKey: string | symbol,
-    descriptor: PropertyDescriptor
-) {
-    console.log(target);
-    console.log(propertyKey);
-    console.log(descriptor);
-}
-
 export class SomeThing {
     /**
      * This will do something
-     * @returns {string} Something different string
+     * @returns Something different string
      */
-    @LogMethod
-    public doSomething(): string {
-        return `Hello World@${new Date().toISOString()}`;
+    public doSomething (): string {
+        return `Hello World@${new Date().toISOString()}`
     }
 }
 
 export default {
-    SomeThing,
+    SomeThing
 }
--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -3,12 +3,12 @@ import { assert } from 'chai'
 
 import { SomeThing } from '../src/index'
 
-describe("class Something", () => {
-    describe("function doSomething", () => {
-        it("should work", () => {
-            const s = new SomeThing();
-            const result = s.doSomething();
-            assert.isString(result);
+describe('class Something', () => {
+    describe('function doSomething', () => {
+        it('should work', () => {
+            const s = new SomeThing()
+            const result = s.doSomething()
+            assert.isString(result)
         })
     })
 })
